d14090cc2dbf4281d9243c38b3de4e06b8fb7bda
duyhotel4vn <duyho@tel4vn.com>
2024-02-23 23:16:08 +0700

    Update - handle histories change

--- service/histories.go
+++ service/histories.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math"
 	"sfvn-hbduy/common/cache"
 	"sfvn-hbduy/common/model"
 	"sfvn-hbduy/common/response"
@@ -38,7 +39,7 @@ func (s *Histories) GetHistories(ctx context.Context, symbol string, days int, p
 }
 
 func (s *Histories) cacheHistories(ctx context.Context, symbol string, days int, period string) ([]model.HistoricalData, error) {
-	key := fmt.Sprintf("cache_histories_%s_%d_%s", symbol, days, period)
+	key := fmt.Sprintf("cache_histories_%s_%d", symbol, days)
 	var historicalData []model.HistoricalData
 	if historiesCached, err := cache.MCache.Get(key); err != nil || historiesCached == nil {
 		result, err := util.CallAPI("GET", s.BaseURL+"/coins/"+symbol+"/ohlc", map[string]string{
@@ -55,15 +56,22 @@ func (s *Histories) cacheHistories(ctx context.Context, symbol string, days int,
 			return historicalData, err
 		}
 		var historicalDataTmp []model.HistoricalData
+		var orginNumber float64
+		var percentageChange float64
 		for _, item := range historiesResp {
+			change := math.Abs(orginNumber - item[4])
+			if orginNumber != 0 {
+				percentageChange = (change / orginNumber) * 100
+			}
 			historicalDataTmp = append(historicalDataTmp, model.HistoricalData{
 				High:   item[2],
 				Low:    item[3],
 				Open:   item[1],
 				Close:  item[4],
 				Time:   int64(item[0]),
-				Change: 0,
+				Change: percentageChange,
 			})
+			orginNumber = item[4]
 		}
 		historicalData = historicalDataTmp
 
